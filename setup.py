f6888d164bb9d946781e058618c76fcd177d4969
Nick Smith <portification@gmail.com>
2013-12-10 20:05:12 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+	name='dokus',
+	version='0.0.1',
+	description='TorqueScript documentation generator',
+	author='portify',
+	author_email='portification@gmail.com',
+	url='https://github.com/portify/dokus',
+	packages=['dokus']
+)
