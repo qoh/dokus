ea6b6c7168806f5a09aa4842d1cf608c95b22117
Nick Smith <portification@gmail.com>
2013-12-10 20:20:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,20 @@ setup(
 	name='dokus',
 	version='0.0.1',
 	description='TorqueScript documentation generator',
+	long_description='Dokus is a Python utility and package for generating HTML documentation or extracting data from a source tree, file or blob.',
 	author='portify',
 	author_email='portification@gmail.com',
 	url='https://github.com/portify/dokus',
-	packages=['dokus']
+	license='MIT',
+	packages=['dokus'],
+
+	classifiers=[
+		'Development Status :: 2 - Pre-Alpha',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python',
+		'Programming Language :: Other',
+		'Topic :: Documentation',
+		'Topic :: Software Development :: Documentation'
+	]
 )
